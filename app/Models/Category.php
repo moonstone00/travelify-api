ac1a33caab2bef7d765e44277df6940c687e3d7e
Ghifani <ghifani03@gmail.com>
2025-01-21 19:49:50 +0700

    🗃️ Relationship
    Relationship one to many and one to many insers

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -12,4 +12,8 @@ class Category extends Model
     protected $fillable = [
         'name', 'slug', 'image'
     ];
+
+    public function place() {
+        return $this->hasMany(Place::class);
+    }
 }
--- app/Models/Place.php
+++ app/Models/Place.php
@@ -12,4 +12,17 @@ class Place extends Model
     protected $fillable = [
         'title', 'slug', 'user_id', 'category_id', 'description', 'phone', 'website', 'office_hours', 'address', 'longitude','latitude'
     ];
+
+    // Agar kita juga bisa memanggil data user data data place
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+
+    public function category() {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function placeImage() {
+        return $this->hasMany(PlaceImage::class);
+    }
 }
