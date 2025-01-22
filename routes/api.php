d5b823e33f7afa16278cbbef4e0b2e1b5500ed28
Ghifani <ghifani03@gmail.com>
2025-01-22 17:25:09 +0700

    🛂 Auth admin
    Auth Admin

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// group route with prefix "admin"
+Route::prefix('admin')->group(function () {
+    // route login
+    Route::post('/login', App\Http\Controllers\Api\Admin\LoginController::class, ['as' => 'admin']);
+
+    // group route with middleware "auth:api"
+    Route::group(['middleware' => 'auth:api'], function () {
+
+        // route user logged in
+        Route::get('/user', function (Request $request) {
+            return $request->user();
+        })->name('user');
+    });
 });
+
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
