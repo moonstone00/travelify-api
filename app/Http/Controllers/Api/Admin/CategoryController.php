282f43dd73fa17cfcdfc96314de414297b302197
Ghifani <ghifani03@gmail.com>
2025-01-24 18:06:10 +0700

    👽️ Create Api Categories
    Create update and delete

--- app/Http/Controllers/Api/Admin/CategoryController.php
+++ app/Http/Controllers/Api/Admin/CategoryController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Resources\CategoryResource;
 use App\Models\Category;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Str;
 
@@ -62,5 +63,60 @@ class CategoryController extends Controller
 
         // return failed with Api Resource
         return new CategoryResource(false, 'Detail Data Category Tidak Ditemukan!', null);
+    }
+
+    public function update(Request $request, Category $category) {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|unique:categories,name,'.$category->id,
+        ]);
+
+        if($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        // check image updated
+        if($request->file('image')) {
+
+            // remove old image
+            Storage::disk('local')->delete('public/categories/'.basename($category->image));
+
+            // upload new image
+            $image = $request->file('image');
+            $image->storeAs('public/categories', $image->hashName());
+
+            // update category with new image
+            $category->update([
+                'image' => $image->hashName(),
+                'name' => $request->name,
+                'slug' => Str::slug($request->name, '-'),
+            ]);
+        }
+
+        // update category without image
+        $category->update([
+            'name' => $request->name,
+            'slug' => Str::slug($request->name, '-'),
+        ]);
+
+        if($category) {
+            // return success with Api Resource
+            return new CategoryResource(true, 'Data Category Berhasil Diupdate!', $category);
+        }
+
+        // return failed with Api Resource
+        return new CategoryResource(false, 'Data Category Gagal Diupdate!', null);
+    }
+
+    public function destroy(Category $category) {
+        // remove image
+        Storage::disk('local')->delete('public/categories/'.basename($category->image));
+
+        if($category->delete()) {
+            // return success with Api Resource
+            return new CategoryResource(true, 'Data Category Berhasil Dihapus!', null);
+        }
+
+        // return failed with Api Resource
+        return new CategoryResource(false, 'Data Category Gagal Dihapus!', null);
     }
 }
